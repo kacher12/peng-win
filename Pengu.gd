38417928aa00476c5cd47b60de439969dcb2462e
Pickle Chungus <159173038+milkmenhn@users.noreply.github.com>
2026-04-18 10:48:21 +0200

    dodałem dasha (działa okropnie)
    śmieszne

--- Pengu.gd
+++ Pengu.gd
@@ -7,5 +7,12 @@ func get_input():
 	velocity = input_direction * speed
 
 func _physics_process(delta):
+	
 	get_input()
+	
+	if Input.is_action_just_pressed("dash"):
+		
+		velocity = Input.get_vector("left", "right", "up", "down") * 10000
+	
+
 	move_and_slide()
